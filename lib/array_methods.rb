b2fa522ed956e4c84e23abc29e0b408870daa984
Ayana Bando <ayanaflax@yahoo.com>
2018-02-11 23:54:19 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def using_include(array, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-element = array
-array.include?(element)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  element = array
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.sort
 end
 
 def using_reverse(array)
